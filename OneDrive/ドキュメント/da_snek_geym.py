4564388e619ce79aa5d3216748ba6d040460be90
Sam Oreste <samoreste50@gmail.com>
2024-10-04 21:52:56 +0800

    Creating Variables

---
+++ OneDrive/ドキュメント/da_snek_geym.py
@@ -0,0 +1,14 @@
+from cProfile import label
+from tabnanny import check
+from tkinter import *
+import random
+from webbrowser import Elinks
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 100
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#ADD8E6'
+FOOD_COLOR = '#FF0000'
+BACKGROUND_COLOR = '#000000'
